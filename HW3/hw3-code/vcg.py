775b212f11d5d12b6386f41e907eff18436a2d0a
Andrew Wang <wang.andrew.d@gmail.com>
2013-02-26 21:37:48 -0500

    VCG code

--- HW3/hw3-code/vcg.py
+++ HW3/hw3-code/vcg.py
@@ -53,6 +53,10 @@ class VCG:
             n = len(allocation)
 
             # TODO: Compute the payment and return it.
+            if slot k == len(c):
+                return c[k] * max(reserve, bids[k+1])
+            else:
+                return (c[k] - c[k+1]) * bids[k+1] + total_payment(k+1)
 
         def norm(totals):
             """Normalize total payments by the clicks in each slot"""
